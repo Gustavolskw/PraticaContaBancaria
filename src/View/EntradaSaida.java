2f2f2bcc445861ac25dccb11a26e7c5f41314ce7
Gustavolskw <gustavolschmidt13@gmail.com>
2023-11-28 19:32:59 -0300

    initial commit

---
+++ src/View/EntradaSaida.java
@@ -0,0 +1,14 @@
+package View;
+
+public class EntradaSaida {
+	 public static int solicitarInformacoesDeposito(){
+		
+	 }
+	 public static int solicitarInformacoesSaque(){
+		
+	 }
+	 public static void exibirExtrato(){
+		
+	 }
+	 
+}
